8f335d080724596b608937ea5d8f386d6f1815e8
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2023-04-28 15:36:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
+// Define a chave da API e a URL base para chamar a API do OpenWeatherMap
 const apiKey = "0527d1e8f3c70206230562d177977c3b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+// Obtém as referências para o campo de entrada de pesquisa e o botão de pesquisa
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search a");
+
+// Obtém a referência para o elemento da imagem do ícone do clima
 const weatherIcon = document.querySelector(".weather-icon");
 
+// Define a função async para buscar informações climáticas com base no nome da cidade
 async function checkWeather(city) {
+    // Chama a API do OpenWeatherMap com base na URL e na chave da API, juntamente com o nome da cidade fornecido
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
+    // Se a resposta da API for 404, mostra a mensagem de erro e oculta o conteúdo da previsão do tempo
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    } else {
-
+    } 
+    // Caso contrário, analisa a resposta da API e atualiza os elementos HTML relevantes com as informações climáticas
+    else {
         var data = await response.json();
 
-        const cityElem = document.querySelector(".city");
-        if (cityElem) {
-            cityElem.innerHTML = data.name ?? "Unknown city";
-        }
+        // Atualiza o elemento HTML com a cidade em questão
+        document.querySelector(".city").innerHTML = data.name;
 
-        const tempElem = document.querySelector(".temp");
-        if (tempElem) {
-            tempElem.innerHTML = Math.round(data.main?.temp) + "°c";
-        }
+        // Arredonda e atualiza o elemento HTML com a temperatura em graus Celsius
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
 
-        const humidityElem = document.querySelector(".humidity");
-        if (humidityElem) {
-            humidityElem.innerHTML = data.main?.humidity + "%";
-        }
+        // Atualiza o elemento HTML com a umidade relativa do ar em porcentagem
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
 
-        const windElem = document.querySelector(".wind");
-        if (windElem) {
-            windElem.innerHTML = data.wind?.speed + " km/h";
-        }
+        // Atualiza o elemento HTML com a velocidade do vento em km/h
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
+        // Atualiza o ícone do clima com base na descrição do tempo
         if (data.weather[0].main == 'Clouds') {
             weatherIcon.src = "images/clouds.png"
         }
@@ -51,20 +52,15 @@ async function checkWeather(city) {
             weatherIcon.src = "images/mist.png"
         }
 
+        // Exibe o conteúdo da previsão do tempo e oculta a mensagem de erro (se houver)
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
     }
 }
 
-
+// Adiciona um ouvinte de eventos ao botão de pesquisa para chamar a função checkWeather() com base no valor da caixa de pesquisa
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
 
-searchBox.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        const searchValue = searchBox.value;
-        checkWeather(searchValue);
-    }
-});
+// Adiciona um ouvinte de eventos à caixa de pesquisa para permitir que o usuário pressione a tecla Enter para chamar a função checkWeather() com base no
