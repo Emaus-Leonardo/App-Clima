ac793dfcac20fdb2c8fa763dd9b589e5b4a3492f
Your Name <emausleonardo123@gmail.com>
2024-04-15 15:14:03 -0300

    adicionando feature ao js e css

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const apiKey = "0527d1e8f3c70206230562d177977c3b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+// Obtém o elemento de entrada de pesquisa e o container do Search pelo ID
+const searchInput = document.getElementById('search-input');
+const searchContainer = document.getElementById('search');
+
 // Obtém as referências para o campo de entrada de pesquisa e o botão de pesquisa
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search a");
@@ -18,7 +22,7 @@ async function checkWeather(city) {
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    } 
+    }
     // Caso contrário, analisa a resposta da API e atualiza os elementos HTML relevantes com as informações climáticas
     else {
         var data = await response.json();
@@ -66,9 +70,23 @@ searchBtn.addEventListener("click", () => {
 // Adiciona um ouvinte de evento ao input da barra de pesquisa, que chama a função checkWeather quando a tecla Enter é pressionada
 searchBox.addEventListener('keydown', function (event) {
     if (event.keyCode === 13) {
-      event.preventDefault();
-      const searchValue = searchBox.value;
-      checkWeather(searchValue);
+        event.preventDefault();
+        const searchValue = searchBox.value;
+        checkWeather(searchValue);
     }
-  });
+});
 
+// Adiciona um ouvinte de evento para quando o campo de entrada recebe foco
+searchInput.addEventListener('focus', function () {
+    // Define a largura do contêiner de pesquisa como 100% quando o campo de entrada está em foco
+    searchContainer.style.width = '100%';
+});
+
+// Adiciona um ouvinte de evento para quando o campo de entrada perde foco
+searchInput.addEventListener('blur', function () {
+    // Verifica se o valor do campo de entrada está vazio
+    if (searchInput.value === '') {
+        // Se o campo estiver vazio, define a largura do contêiner de pesquisa de volta para 210px
+        searchContainer.style.width = '210px';
+    }
+});
